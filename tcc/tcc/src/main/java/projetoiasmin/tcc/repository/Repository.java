e8cce84157fa726eebf61b5f1556e6ffb1d7eb92
João Emanuel Pessoa do Nascimento <joao.emanuel.pn@gmail.com>
2024-12-01 21:25:06 -0300

    Delete tcc/tcc directory

--- tcc/tcc/src/main/java/projetoiasmin/tcc/repository/Repository.java
+++
@@ -1,7 +0,0 @@
-package projetoiasmin.tcc.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import projetoiasmin.tcc.model.Model;
-
-public interface Repository extends JpaRepository<Model, Long> {
-}
